d7b5f77b555dca0a93de13e4f5e0acddb50dcc05
Семенов Вадим Сергеевич <Semenov.Vadim_S@russianpost.ru>
2025-04-27 20:02:37 +0300

    Add changes

--- SimpleDapperAOT.DataAccess/Connection/ConnectionString/Postgres/PostgresConnectionString.cs
+++ SimpleDapperAOT.DataAccess/Connection/ConnectionString/Postgres/PostgresConnectionString.cs
@@ -8,5 +8,5 @@ public class PostgresConnectionString : IConnectionStringStorage
     public DbType DbType => DbType.Postgres;
     // public string ConnectionString => "Driver={PostgreSQL ODBC Driver(UNICODE)};Server=127.0.0.1;Port=5432;Database=postgres;UID=postgres;PWD=131564471";
     // public string ConnectionString => "Host=localhost;Port=5432;Database=postgres;Username=postgres;Password=131564471";
-    public string ConnectionString => "Host=localhost;Port=5432;Database=postgres;User Id=postgres;Password=131564471";
+    public string ConnectionString => "Host=localhost;Port=5432;Database=postgres";
 }
--- SimpleDapperAOT.DataAccess/Connection/ConnectionString/SqlServer/SqlServerConnectionString.cs
+++ SimpleDapperAOT.DataAccess/Connection/ConnectionString/SqlServer/SqlServerConnectionString.cs
@@ -6,5 +6,5 @@ namespace SimpleDapperAOT.DataAccess.Connection.ConnectionString.SqlServer;
 public class SqlServerConnectionString : IConnectionStringStorage
 {
     public DbType DbType => DbType.SqlServer;
-    public string ConnectionString => "Server=localhost;Database=MyDb;User Id=sa;Password=131564471;TrustServerCertificate=True;";
+    public string ConnectionString => "Server=localhost;Database=MyDb;TrustServerCertificate=True;";
 }
